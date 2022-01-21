b99331086ced3a278b59603869e32ffeb4845d5e
Maria Guchkova <mariaguchkova1@gmail.com>
2022-01-21 16:55:44 +0300

    feat: add adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,14 +1,35 @@
 (function() {
     // "use strict";
     let toggles = document.querySelectorAll(".menu-bg");
-    for (let i = toggles.length - 1; i >= 0; i--) {
+    let links = document.querySelectorAll(".link");
+    let menu = document.querySelector(".menu");
+    for (let i = 0; i <= toggles.length - 1; i++) {
       let toggle = toggles[i];
       toggleHandler(toggle);
     };
     function toggleHandler(toggle) {
-      toggle.addEventListener( "click", function(e) {
+      toggle.addEventListener("click", function(e) {
         e.preventDefault();
-        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+        if (toggle.classList.contains("is-active") === true) {
+            toggle.classList.remove("is-active");
+            menu.classList.remove("open");
+        } else {
+            toggle.classList.add("is-active");
+            menu.classList.add("open");
+        } 
       });
     }
-  })();
+    
+    function closeMenu() {
+        for (let toggle of toggles) {
+            toggle.classList.remove("is-active");
+        }
+        menu.classList.remove("open");
+    }
+    for (let link of links) {
+        link.addEventListener("click", closeMenu);
+    }
+}
+)();
+
+
