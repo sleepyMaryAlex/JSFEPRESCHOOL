0831bdb7c0b393b641f6c57a009f6eac403b80ff
Maria Guchkova <mariaguchkova1@gmail.com>
2022-01-27 08:54:07 +0300

    feat: add styles and images for white theme

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -57,14 +57,64 @@ console.log("");
 (function() {
   let en = document.querySelector(".en");
   let ru = document.querySelector(".ru");
+  let body = document.querySelector(".body");
   let languages = document.querySelectorAll(".lng");
   let data = document.querySelectorAll("[data-i18]");
   function getTranslate(e) {
     let lang = e.target.textContent;
     data.forEach((elem) => elem.textContent = i18Obj[lang][elem.dataset.i18]);
-    languages.forEach((element) => element.classList.remove("active-lng"));
-    e.target.classList.add("active-lng");
-  }
+    if (body.classList.contains("light-theme")) {
+      languages.forEach((element) => element.classList.remove("active-lng"));
+      languages.forEach((element) => element.classList.remove("active-white"));
+      e.target.classList.add("active-white");
+    } else {
+      languages.forEach((element) => element.classList.remove("active-lng"));
+      languages.forEach((element) => element.classList.remove("active-white"));
+      e.target.classList.add("active-white");
+      e.target.classList.add("active-lng");
+    }
+
+    }
 en.addEventListener("click", getTranslate, false);
 ru.addEventListener("click", getTranslate, false);
+})();
+
+(function() {
+  let body = document.querySelector(".body");
+  let hero = document.querySelector(".hero");
+  let containerHeader = document.querySelector(".container-header");
+  let contacts = document.querySelector(".contacts");
+  let switchImg = document.querySelector(".switch-img");
+  let en = document.querySelector(".en");
+  let ru = document.querySelector(".ru");
+  
+  function switchTheme(e) {
+    body.classList.toggle("light-theme");
+    hero.classList.toggle("hero-bg");
+    containerHeader.classList.toggle("header-light");
+    contacts.classList.toggle("contacts-light");
+    let imageSrc1 = "assets/svg/sun.svg"
+    let imageSrc2 = "assets/svg/moon.svg"
+    if (switchImg.getAttribute("src") === imageSrc1) {
+      switchImg.setAttribute("src", imageSrc2);
+    } else if (switchImg.getAttribute("src") === imageSrc2) {
+      switchImg.setAttribute("src", imageSrc1);
+    }
+   if (ru.classList.contains("active-lng") && switchImg.getAttribute("src") === imageSrc2) {
+    ru.classList.remove("active-lng");
+    ru.classList.add("active-white");
+   } else if (en.classList.contains("active-lng") && switchImg.getAttribute("src") === imageSrc2){
+    en.classList.remove("active-lng");
+    en.classList.add("active-white");
+   }
+   if (ru.classList.contains("active-white") && switchImg.getAttribute("src") === imageSrc1) {
+    ru.classList.remove("active-white");
+    ru.classList.add("active-lng");
+   } else if (en.classList.contains("active-white") && switchImg.getAttribute("src") === imageSrc1) {
+    en.classList.remove("active-white");
+    en.classList.add("active-lng");
+   }
+   
+  }
+  switchImg.addEventListener("click", switchTheme, false);
 })();
