08f3be158673838742a2df657a68d66feee74ef2
Maria Guchkova <mariaguchkova1@gmail.com>
2022-02-09 17:34:05 +0300

    feat: add slideshow

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let mySlides = document.querySelectorAll(".my-slides");
+  if (n > mySlides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = mySlides.length;
+  }
+  for (let i = 0; i < mySlides.length; i++) {
+      mySlides[i].style.display = "none";
+  }
+  mySlides[slideIndex-1].style.display = "block";
+}
+
+
+
+
+
+
+
+
 let url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aa2ce8765d07b86eafdaaf3467fd8307";
 
 let posters = document.querySelectorAll(".poster")
