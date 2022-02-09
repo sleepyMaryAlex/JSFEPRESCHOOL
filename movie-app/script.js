a09c0527f3b1aea5f55d81d15a5c3f20eedb904c
Maria Guchkova <mariaguchkova1@gmail.com>
2022-02-09 07:33:50 +0300

    feat: add header and footer

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,8 +1,10 @@
 let url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aa2ce8765d07b86eafdaaf3467fd8307";
 
+let posters = document.querySelectorAll(".poster")
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
+  posters.forEach((poster, index) => poster.textContent = data.results[index].overview);
 }
 getData();
