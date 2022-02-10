f771fe2b5d3f46581d574657ebc25dc2eaeea33f
Maria Guchkova <mariaguchkova1@gmail.com>
2022-02-10 18:35:20 +0300

    feat: add search input

--- movie-app/script.js
+++ movie-app/script.js
@@ -2,11 +2,11 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -18,65 +18,118 @@ function showSlides(n) {
     slideIndex = mySlides.length;
   }
   for (let i = 0; i < mySlides.length; i++) {
-      mySlides[i].style.display = "none";
+    mySlides[i].style.display = "none";
   }
-  mySlides[slideIndex-1].style.display = "block";
+  mySlides[slideIndex - 1].style.display = "block";
 }
 
 // ==================================
 
-let url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aa2ce8765d07b86eafdaaf3467fd8307";
+let DEFAULT_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aa2ce8765d07b86eafdaaf3467fd8307";
 
 let mainContainer = document.querySelector(".main-container");
+let input = document.querySelector(".input");
+let cancel = document.querySelector(".cancel");
+let inputValue;
 
-async function getData() {
+async function getData(url) {
   const res = await fetch(url);
   const data = await res.json();
-  for (let i = 0; i < data.results.length; i++) {
-    let div = document.createElement("div");
-    mainContainer.append(div);
-    div.classList.add("poster");
-    let img = document.createElement("img");
-    div.append(img);
-    img.setAttribute("src", `https://image.tmdb.org/t/p/w300${data.results[i].poster_path}`);
-    let p = document.createElement("p");
-    div.append(p);
-    p.textContent = data.results[i].title;
-    p.classList.add("original-title");
-    let pYear = document.createElement("p");
-    div.append(pYear);
-    pYear.textContent = data.results[i].release_date.slice(0, 4);
-    pYear.classList.add("year");
-    let divOverview = document.createElement("div");
-    div.append(divOverview);
-    divOverview.textContent = data.results[i].overview;
-    divOverview.classList.add("overview");
-    div.addEventListener("mouseenter", () => divOverview.classList.add("visible"));
-    div.addEventListener("mouseleave", () => divOverview.classList.remove("visible"));
-    let pVote = document.createElement("p");
-    div.append(pVote);
-    if (data.results[i].vote_average.toString().length === 1) {
-     pVote.textContent = `${data.results[i].vote_average}.0`;
-    } else {
-      pVote.textContent = data.results[i].vote_average;
+  if (data.results.length === 0) {
+    showMessage();
+  } else {
+    for (let i = 0; i < data.results.length; i++) {
+      let div = document.createElement("div");
+      mainContainer.append(div);
+      div.classList.add("poster");
+      let img = document.createElement("img");
+      div.append(img);
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w300${data.results[i].poster_path}`
+      );
+      let p = document.createElement("p");
+      div.append(p);
+      p.textContent = data.results[i].title;
+      p.classList.add("original-title");
+      let pYear = document.createElement("p");
+      div.append(pYear);
+      pYear.textContent = data.results[i].release_date.slice(0, 4);
+      pYear.classList.add("year");
+      let divOverview = document.createElement("div");
+      div.append(divOverview);
+      divOverview.textContent = data.results[i].overview;
+      divOverview.classList.add("overview");
+      div.addEventListener("mouseenter", () =>
+        divOverview.classList.add("visible")
+      );
+      div.addEventListener("mouseleave", () =>
+        divOverview.classList.remove("visible")
+      );
+      let pVote = document.createElement("p");
+      div.append(pVote);
+      if (data.results[i].vote_average.toString().length === 1) {
+        pVote.textContent = `${data.results[i].vote_average}.0`;
+      } else {
+        pVote.textContent = data.results[i].vote_average;
+      }
+      pVote.classList.add("vote");
+      if (data.results[i].vote_average >= 9) {
+        pVote.style.backgroundColor = "#932241";
+      } else if (data.results[i].vote_average >= 8) {
+        pVote.style.backgroundColor = "#267398";
+      } else if (data.results[i].vote_average >= 7) {
+        pVote.style.backgroundColor = "#A6842A";
+      } else if (data.results[i].vote_average >= 6) {
+        pVote.style.backgroundColor = "#27349A";
+      } else if (data.results[i].vote_average >= 5) {
+        pVote.style.backgroundColor = "#000635";
+      } else if (data.results[i].vote_average >= 4) {
+        pVote.style.backgroundColor = "#A66C2A";
+      } else if (data.results[i].vote_average <= 3) {
+        pVote.style.backgroundColor = "#3A2E0F";
+      }
     }
-    pVote.classList.add("vote");
-    if (data.results[i].vote_average >= 9) {
-      pVote.style.backgroundColor = "#932241";
-    } else if (data.results[i].vote_average >= 8) {
-      pVote.style.backgroundColor = "#267398";
-    } else if (data.results[i].vote_average >= 7) {
-      pVote.style.backgroundColor = "#A6842A";
-    } else if (data.results[i].vote_average >= 6) {
-      pVote.style.backgroundColor = "#27349A";
-    } else if (data.results[i].vote_average >= 5) {
-      pVote.style.backgroundColor = "#000635";
-    } else if (data.results[i].vote_average >= 4) {
-      pVote.style.backgroundColor = "#A66C2A";
-    } else if (data.results[i].vote_average <= 3) {
-      pVote.style.backgroundColor = "#3A2E0F";
-    } 
+  }
+}
+getData(DEFAULT_URL);
+
+document.addEventListener("keydown", check);
+cancel.addEventListener("click", clearInput);
+input.addEventListener("input", changeImg);
+function check(e) {
+  if (e.keyCode === 13) {
+    searchMovie();
   }
 }
 
-getData();
+function changeImg() {
+  cancel.setAttribute("src", "assets/cancel.svg");
+}
+
+function clearInput() {
+  if (cancel.getAttribute("src") === "assets/cancel.svg") {
+    cancel.setAttribute("src", "assets/search.svg");
+    input.value = "";
+  }
+}
+
+function showMessage() {
+  let message = document.createElement("p");
+  mainContainer.append(message);
+  message.classList.add("message");
+  message.textContent = "nothing found for your request";
+}
+
+function searchMovie() {
+  mainContainer.innerHTML = "";
+  inputValue = input.value;
+  if (inputValue === "") {
+    showMessage();
+  }
+  searchUrl = `https://api.themoviedb.org/3/search/movie?query=${inputValue}&api_key=aa2ce8765d07b86eafdaaf3467fd8307`;
+  getData(searchUrl);
+}
+
+// https://api.themoviedb.org/3/search/movie?query=hello&api_key=aa2ce8765d07b86eafdaaf3467fd8307"`
