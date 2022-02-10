8fac8ca769a326650342475347d4e068f45e6f9b
Maria Guchkova <mariaguchkova1@gmail.com>
2022-02-10 14:16:23 +0300

    feat: add posters to body

--- movie-app/script.js
+++ movie-app/script.js
@@ -23,20 +23,37 @@ function showSlides(n) {
   mySlides[slideIndex-1].style.display = "block";
 }
 
-
-
-
-
-
-
+// ==================================
 
 let url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aa2ce8765d07b86eafdaaf3467fd8307";
 
-let posters = document.querySelectorAll(".poster")
+let mainContainer = document.querySelector(".main-container");
+
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-  posters.forEach((poster, index) => poster.textContent = data.results[index].overview);
+  for (let i = 0; i < data.results.length; i++) {
+    let div = document.createElement("div");
+    mainContainer.append(div);
+    div.classList.add("poster");
+    let img = document.createElement("img");
+    div.append(img);
+    img.setAttribute("src", `https://image.tmdb.org/t/p/w300${data.results[i].poster_path}`);
+    let p = document.createElement("p");
+    div.append(p);
+    p.textContent = data.results[i].title;
+    p.classList.add("original-title");
+    let pYear = document.createElement("p");
+    div.append(pYear);
+    pYear.textContent = data.results[i].release_date.slice(0, 4);
+    pYear.classList.add("year");
+    let divOverview = document.createElement("div");
+    div.append(divOverview);
+    divOverview.textContent = data.results[i].overview;
+    divOverview.classList.add("overview");
+    div.addEventListener("mouseenter", () => divOverview.classList.add("visible"));
+    div.addEventListener("mouseleave", () => divOverview.classList.remove("visible"));
+  }
 }
+
 getData();
