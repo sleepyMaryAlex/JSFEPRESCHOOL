22f1ca8580441faa275e9fb76816645dc82f77a7
Maria Guchkova <mariaguchkova1@gmail.com>
2022-02-14 14:28:12 +0300

    feat: add checkForMatch function

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,7 +1,49 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+let lockBoard = false;
+
 function flipCard() {
- this.classList.toggle("flip");
+  if (lockBoard) {
+      return;
+  }
+  this.classList.add("flip");
+
+  if (hasFlippedCard === false) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        lockBoard = false;
+    }, 1500);
+}
+
+
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
